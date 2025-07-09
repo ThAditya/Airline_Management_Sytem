b8239921b6861c53e3e66ca6e4ee721c2179b24e
Aditya Vashishth <156231205+ThAditya@users.noreply.github.com>
2025-07-09 11:41:28 +0530

    SQL

---
+++ airlineManagementSystem.sql
@@ -0,0 +1,10 @@
+create database airlineManagementSystem;
+
+USE airlineManagementSystem;
+
+CREATE TABLE LOGIN(
+    USER_NAME VARCHAR(30),
+    PASSWORD VARCHAR(20)
+);
+
+INSERT INTO LOGIN VALUES ("admin", "admin123");
